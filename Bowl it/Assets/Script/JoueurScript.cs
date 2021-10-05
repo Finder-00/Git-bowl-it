4dc82dac491410ba0ae58b131d8169e475cad721
Finder-00 <arthurrobitaille@protonmail.com>
2021-10-04 21:51:17 -0400

    modif affichage texte

--- Bowl it/Assets/Script/JoueurScript.cs
+++ Bowl it/Assets/Script/JoueurScript.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class JoueurScript : MonoBehaviour
 {                                                                                                   
     public static int points;
-    public Text textePoints;
+    public TMP_Text textePoints;
 
 
     void Start()
     {
-        textePoints = GetComponent<Text>();
+        //textePoints = GetComponent<Text>();
         points = 0;
         textePoints.text = "0";
     }
